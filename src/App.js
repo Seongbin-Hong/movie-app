9428fa44fcbc52ac50e334f2654d8b916217982a
Seongbin Hong <qls056@gmail.com>
2021-07-14 22:49:04 +0900

    map 함수를 이용해서 component 재사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Sample from "./Sample";
+
+function Sample({ name }) {
+    return <h3>This is {name}</h3>;
+}
+
+const sampleList = [
+    { id: 1, name: "A" },
+    { id: 2, name: "B" },
+    { id: 3, name: "C" },
+    { id: 4, name: "D" },
+];
+
+function renderSample(item) {
+    return <Sample key={item.id} name={item.name} />;
+}
 
 function App() {
     return (
         <div>
             <h2>Hello!</h2>
-            <Sample />
+            {sampleList.map(renderSample)}
         </div>
     );
 }
