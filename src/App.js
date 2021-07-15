fdc380a06f91de9d58d8a4d065171c2a41b6cee8
Seongbin Hong <qls056@gmail.com>
2021-07-15 18:20:12 +0900

    Function component에서 class component로 변경
        * state 추가
        * setState 함수로 render 재실행
        * setState current로 state 확인 후 변경 (add, minus)

--- src/App.js
+++ src/App.js
@@ -1,69 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const sampleList = [
-    {
-        id: 1,
-        name: "A",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAX3q2a4UQWPdtCyRlQ0x3K739NnSbBGlLww&usqp=CAU",
-        rating: 3.3,
-    },
-    {
-        id: 2,
-        name: "B",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWlUy0dR8BwoUj_qcn0dsun0B-BhvDE4bs4g&usqp=CAU",
-        rating: 2.1,
-    },
-    {
-        id: 3,
-        name: "C",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT84PDno6VM3jICtnbLLJARMtN4FqxjM_joqQ&usqp=CAU",
-        rating: 4.5,
-    },
-    {
-        id: 4,
-        name: "D",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1W1mW10fO564H87A2mQ7IaNyi0Rmi4xBFlg&usqp=CAU",
-        rating: 4.3,
-    },
-    {
-        id: 5,
-        name: "E",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgGifpWxee7EoLtgJncUNaOA-GNh2N1uekAA&usqp=CAU",
-        rating: 3.1,
-    },
-];
-
-function Dog({ name, image, rating }) {
-    return (
-        <div>
-            <h4>
-                This is {name}
-                <span> {rating}/5.0</span>
-            </h4>
-            <img src={image} alt={name} />
-        </div>
-    );
-}
-
-Dog.propTypes = {
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string,
-    rating: PropTypes.number,
-};
-
-function renderDog(item) {
-    return (
-        <Dog
-            key={item.id}
-            name={item.name}
-            image={item.image}
-            rating={item.rating}
-        />
-    );
-}
-
-// Main function
+/*// function component
 function App() {
     return (
         <div>
@@ -71,6 +9,29 @@ function App() {
             {sampleList.map(renderDog)}
         </div>
     );
+}*/
+
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
+    add = () => {
+        console.log("add");
+        this.setState((current) => ({ count: current.count + 1 }));
+    };
+    minus = () => {
+        console.log("minus");
+        this.setState((current) => ({ count: current.count - 1 }));
+    };
+    render() {
+        return (
+            <div>
+                <h1>The number : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
 export default App;
